ab1a2295f28afed90d84ca3a091e6e6d991b74dc
jiangyitao <451988022@163.com>
2018-12-23 23:14:37 +0800

    完成agent列表功能

--- src/views/agent/index.vue
+++ src/views/agent/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="app-container">
-    agent
+    <el-button style="margin-bottom: 5px" icon="el-icon-refresh" size="mini" @click="getOnlineAgents"></el-button>
+    <el-table :data="agents" highlight-current-row border>
+      <el-table-column label="状态" align="center" width="100">
+        <template scope="scope">
+          <div class="circle"></div>
+        </template>
+      </el-table-column>
+      <el-table-column label="agent" align="center">
+        <template scope="scope">
+          {{ scope.row.agentIp + ":" + scope.row.agentPort }}
+        </template>
+      </el-table-column>
+      <el-table-column label="设备" align="center">
+        <template scope="scope">
+          <el-table :data="scope.row.devices" size="mini">
+            <el-table-column label="设备名" prop="deviceName" align="center"></el-table-column>
+            <el-table-column label="设备id" prop="deviceId" align="center"></el-table-column>
+            <el-table-column label="设备状态" align="center">
+              <template scope="scope1">
+                {{ scope1.row.status === 1 ? "在线闲置":"使用中" }}
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -17,6 +42,7 @@ export default {
   methods:{
     getOnlineAgents(){
       listOfOnline().then(resp=>{
+        this.agents = resp.data
       })
     }
   },
@@ -25,3 +51,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .circle {
+    margin: 0 auto;
+    width: 10px;
+    height: 10px;
+    background-color:#42b983;
+    border-radius: 50%;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+  }
+</style>
